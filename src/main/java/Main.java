d63f3000335b68903d00e8cd5a6efede2bbd078b
Allen L <allenl@Zipcoders-MBP.fios-router.home>
2022-02-25 00:24:55 -0500

    started it

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+        int input = in.nextInt();
+        System.out.println("Guess a number between 1-100");
+        if (input < number) {
+            System.out.println("Guess too low!");
+        }
+        if (input == number) {
+            System.out.println();
+        }
+        if (input > number) {
+            System.out.println("Guess too high");
+        }
     }
 }
